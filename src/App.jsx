e8cdc451949f3840174c91723c2f97487f037417
Cezar D Silva <cezardsilva@gmail.com>
2025-05-11 06:47:55 -0300

    Inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [likes, setLikes] = useState(0)
+  const [input, setInput] = useState('')
+
+  function darOLike(){
+    setLikes(prevLikes => prevLikes + 1)
+    
+    console.log(likes)
+  }
+
+
+  return (
+    <div>
+      <p>QTD de Likes: {likes}</p>
+      <button onClick={darOLike}>Like</button>
+    
+      <p>O que você digitou no input: {input}</p>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+    </div>
+  )
+}
+
+export default App
